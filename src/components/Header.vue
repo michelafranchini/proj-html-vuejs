37a7ac3ca3c1a2396e401cc4dc2ca886ee26973f
michelafranchini <michela.franchini.91@gmail.com>
2021-06-21 11:46:18 +0200

    Macrostruttura

---
+++ src/components/Header.vue
@@ -0,0 +1,29 @@
+<template>
+  <header>
+      <div class="bg_blue">
+          <div class="container_75">
+            <p>Evertything about Lifestyle, Travel and Gadgets!</p>
+          </div>
+      </div>
+  </header>
+</template>
+
+<script>
+export default {
+    name: "Header"
+}
+</script>
+
+<style lang="scss" scoped>
+    .bg_blue {
+        height: 50px;
+        color: white;
+        background-color: #0088cc;
+    }
+
+    .container_75 {
+        width: 75%;
+        height: 50px;
+        margin: auto;
+    }
+</style>
